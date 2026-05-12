b9e69cf262116388433f6ac352e7a04981cbb24d
tiekuu <2760909443@qq.com>
2026-05-12 23:55:06 +0800

    替换pause界面

--- Assets/scripts/TekuuNew/SceneTransitionManager.cs
+++ Assets/scripts/TekuuNew/SceneTransitionManager.cs
@@ -14,6 +14,10 @@ public class SceneTransitionManager : MonoBehaviour
         //记录当前场景
         previousScene = SceneManager.GetActiveScene().name;
         Time.timeScale = 1f;
+        if (sceneName == "MainMenu")
+        {
+            SoundManager.instance.Play(0, "BGM_1", true);
+        }
         SceneManager.LoadScene(sceneName);
     }
 
