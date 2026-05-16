75374041e752b6761a155770392d03f985c6452d
Maxyjof <3478124403@qq.com>
2026-05-16 15:07:54 +0800

    修复
    修复了ESC可以提前触发的问题

--- Assets/Scripts/Trigger/EasterEggTrigger.cs
+++ Assets/Scripts/Trigger/EasterEggTrigger.cs
@@ -22,5 +22,15 @@ public class EasterEggTrigger : MonoBehaviour
         }
     }
 
-    public void ChangeText() { Text.text = Texts[_index++]; }
+    public void ChangeText()
+    {
+        if (_index >= Texts.Count)
+        {
+            ShowPanel.SetActive(false);
+            _index = 0;
+            Text.text = "“恭喜你人类！发现了这个彩蛋！这个彩蛋没有任何提示，任何奖励，任何有用信息。但我相信你看得出来的，这样小小的空间之中居然装满了来自创作人们真挚的心^_^，而当你发现了这处空间，当你阅读这些文字，当你咀嚼它们的用意之时，我们的决心便已经不再局限于这方天地，我们的理想便不再局限于即见所得的现实”\n——铁奎";
+        }
+        
+        Text.text = Texts[_index++];
+    }
 }
