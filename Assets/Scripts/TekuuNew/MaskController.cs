062457eb6ddae4a807e5dd0114b613c11b007a2b
Maxyjof <3478124403@qq.com>
2026-05-16 14:19:05 +0800

    修复
    修复设置界面的问题

--- Assets/Scripts/TekuuNew/MaskController.cs
+++ Assets/Scripts/TekuuNew/MaskController.cs
@@ -6,23 +6,29 @@ public class MaskController : MonoBehaviour
 {
     [SerializeField] private GameObject settingPanel; // 蒙版 Panel 对象
     [SerializeField] private GameObject objectwithShow;
-    [SerializeField] private GameObject objectWithHide;
+    [SerializeField] private List<GameObject> objectsWithHide;
 
     // 显示蒙版
     public void ShowMask()
     {
         settingPanel.SetActive(true);
-
         objectwithShow.SetActive(true);
-        objectWithHide.SetActive(false);
+        
+        foreach (var objectWithHide in objectsWithHide)
+        {
+            objectWithHide.SetActive(false);
+        }
     }
 
     // 隐藏蒙版
     public void HideMask()
     {
         settingPanel.SetActive(false);
-
         objectwithShow.SetActive(false);
-        objectWithHide.SetActive(true);
+        
+        foreach (var objectWithHide in objectsWithHide)
+        {
+            objectWithHide.SetActive(true);
+        }
     }
 }
