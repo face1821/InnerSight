acaee7dd8b7781f1080126edecb4052a5c7c781f
Maxyjof <3478124403@qq.com>
2026-05-16 14:59:55 +0800

    修复，添加
    修复了关卡问题
    
    添加了彩蛋

---
+++ Assets/Scripts/Trigger/EasterEggTrigger.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EasterEggTrigger : MonoBehaviour
+{
+    public GameObject ShowPanel;
+    public TextMeshProUGUI Text;
+
+    [TextArea]
+    public List<string> Texts;
+    private int _index;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("MainPlayer"))
+        {
+            Debug.LogWarning("彩蛋");
+
+            ShowPanel.SetActive(true);
+        }
+    }
+
+    public void ChangeText()
+    {
+        Text.text = Texts[_index++];
+    }
+}
